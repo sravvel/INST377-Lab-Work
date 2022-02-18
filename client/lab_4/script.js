db7764922027e55d5ba02469df0cf5cd3e8485d8
sravvel <srav.vel@gmail.com>
2022-02-17 21:37:22 -0500

    changes to lab 4

--- client/lab_4/script.js
+++ client/lab_4/script.js
@@ -1,3 +1,30 @@
 let slidePosition = 0;
-const slides = document.querySelectorAll('carousel_item');
+const slides = document.querySelectorAll('.carousel_item');
 const totalSlides = slides.length;
+
+document.querySelector('#carousel_button--next').addEventListener("click", function() { 
+    moveToNextSlide();
+});
+
+document.querySelector('#carousel_button--prev').addEventListener("click", function() {
+  moveToPrevSlide();
+});
+
+
+
+/*19:50*/
+function updateSlidePosition()
+
+function moveToNextSlide() {
+    /*alert("hello next");*/
+    if (slidePosition == totalSlides - 1){
+        slidePosition = 0;
+    } else {
+        slidePosition++;
+    }
+}
+
+function moveToPrevSlide() {
+    alert("hello prev");
+
+}
