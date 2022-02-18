8c287e28ce8b2d8f8d60347c767d3cba24ab6946
sravvel <srav.vel@gmail.com>
2022-02-17 20:19:49 -0500

    changes to lab 4

--- client/lab_4/script.js
+++ client/lab_4/script.js
@@ -1,8 +1,3 @@
 let slidePosition = 0;
 const slides = document.querySelectorAll('carousel_item');
 const totalSlides = slides.length;
-
-document.querySelector('carousel_button--next').addEventListener("click", function() {
-    moveToNextSlide();
-
-});
