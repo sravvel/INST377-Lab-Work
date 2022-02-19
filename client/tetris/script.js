a3517dcfbbed713f96e65f47ed58a2f596710942
sravvel <srav.vel@gmail.com>
2022-02-18 21:00:05 -0500

    changes to tetris

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -44,18 +44,107 @@ const lTetromino = [
 
   const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino];
 
-  let currentposition = 4;
-  let current = theTetrominoes[0][0];
+  let currentPosition = 4;
+  let currentRotation = 0;
+
+//randomly select a Tet and its first rotation
+let random = Math.floor(Math.random()*theTetrominoes.length);
+  let current = theTetrominoes[random][currentRotation];
 
 
 //draw the Tetromino
 function draw() {
     current.forEach(index => {
-      squares[currentPosition + index].classList.add('tetromino')
+      squares[currentPosition + index].classList.add('tetromino');
     })
   }
 
-  draw ()
+ //undraw the Tet
+ function undraw() {
+    current.forEach(index => {
+      squares[currentPosition + index].classList.remove('tetromino');
+
+    })
+  }
+
+timerId = setInterval(moveDown, 1000);
+
+// assign functions to keyCodes
+function control(e) {
+    if(e.keyCode === 37) {
+        moveLeft();
+    } else if (e.keyCode === 38) {
+        //rotate()
+    } else if (e.keyCode === 39) {
+        moveRight();
+      } else if (e.keyCode === 40) {
+        moveDown();
+      }
+} 
+document.addEventListener('keyup', control);
+
+function moveDown() {
+    undraw();
+    currentPosition += width;
+    draw();
+    freeze();
+  }
+
+  function freeze() {
+    if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
+    current.forEach(index => squares[currentPosition + index].classList.add('taken'));
+    random = Math.floor(Math.random() * theTetrominoes.length);
+    current = theTetrominoes[random][currentRotation];
+    currentPosition = 4;
+    draw();
+    }
+  }
+
+  //move tets left, unless it is at edge or blockage
+  function moveLeft() {
+    undraw();
+    const isAtLeftEdge = current.some(index => (currentPosition + index) % width === 0);
+
+    if (!isAtLeftEdge) currentPosition -= 1;
+
+    if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+        currentPosition += 1;
+    }
+
+    draw();
+  }
+
+//move tet right, unless is at the edge or there is a blockage
+function moveRight() {
+    undraw();
+    const isAtRightEdge = current.some(index => (currentPosition + index) % width === width -1);
+    if(!isAtRightEdge) currentPosition += 1;
+    if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+      currentPosition -= 1;
+    }
+    draw();
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
