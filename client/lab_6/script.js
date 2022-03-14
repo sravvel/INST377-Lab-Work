e7939fd6e8bc4e93d8d3b8e072c9db8c3a01b58e
sravvel <srav.vel@gmail.com>
2022-03-13 21:10:25 -0400

    changes to lab 6

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -7,13 +7,13 @@ function getRandomIntInclusive(min, max) {
 
 function restoArrayMake(dataArray) {
   console.log('fired datahandler');
-  console.table(dataArray); // this is called "dot notation"
+  // console.table(dataArray); // this is called "dot notation"
   const range = [...Array(15).keys()];
   const listItems = range.map((item, index) => {
     const restNum = getRandomIntInclusive(0, dataArray.length - 1);
     return dataArray[restNum];
   });
-  console.log(listItems);
+  // console.log(listItems);
   return listItems;
   // forEach for reference
   // range.forEach((item) => {
@@ -21,22 +21,36 @@ function restoArrayMake(dataArray) {
   // });
 }
 
+function createHtmlList(collection) {
+  console.log('fired HTML creator');
+  console.log(collection);
+  const targetList = document.querySelector('.resto-list');
+  targetList.innerHTML = '';
+  collection.forEach((item) => {
+    const injectThisItem = `<li>${item.name}</li>`;
+    targetList.innerHTML += injectThisItem;
+  });
+}
+
 async function mainEvent() { // the async keyword means we can make API requests
   const form = document.querySelector('.speaker-form');
-  // const submit = document.querySelector('.submit-button');
+  const submit = document.querySelector('.submit_button');
+  submit.style.display = 'none';
+
   const results = await fetch('/api/foodServicesPG'); // This accesses some data from our API
   const arrayFromJson = await results.json(); // This changes it into data we can use - an object
   console.log(arrayFromJson);
+
   if (arrayFromJson.data.length > 0) {
-    // submit.style.display = 'block';
+    submit.style.display = 'block';
     form.addEventListener('submit', async (submitEvent) => { // async has to be declared all the way to get an await
       submitEvent.preventDefault(); // This prevents your page from refreshing!
       console.log('form submission'); // this is substituting for a "breakpoint"
       // arrayFromJson.data - we're accessing a key called 'data' on the returned object
       // it contains all 1,000 records we need
       const restoArray = restoArrayMake(arrayFromJson.data);
+      createHtmlList(restoArray);
     });
-  // const results = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
   }
 }
 // this actually runs first! It's calling the function above
