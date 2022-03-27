0852d21599b355ceaf99abb3b23619ebcd8ba065
sravvel <98434706+sravvel@users.noreply.github.com>
2022-03-26 20:32:05 -0400

    changes to js

--- client/lab_7/script.js
+++ client/lab_7/script.js
@@ -44,20 +44,32 @@ function getRandomIntInclusive(min, max) {
     const arrayFromJson = await results.json(); // This changes it into data we can use - an object
     // console.log(arrayFromJson);
   
+
+
+
+
+
+
     // prevent race condition on data load
-    if (arrayFromJson.data.length > 0) { 
+    if (arrayFromJson.data.length > 0) {
       submit.style.display = 'block';
 
-
+// allows us to change the var to anything, but pre-sets as array
       let currentArray = [];
-      resto.addEventListener('input', async (event)=>{
-        if (currentArray === undefined) { return; }
+      resto.addEventListener('input', async (event) => {
         console.log(event.target.value);
-        const matchResto = currentArray.filter((item) => {
-          console.log(item.name);
-          return item.name.includes(event.target.value);
-        });
-        console.log(matchResto);
+
+        if (currentArray.length < 1) {
+          return;
+        }
+
+    const selectResto = currentArray.filter((item) => {
+      const lowerName = item.name.toLowerCase();
+      const lowerValue = event.target.value.toLowerCase();
+      return lowerName.includes(lowerValue);
+    });
+        console.log(selectResto);
+        createHtmlList(selectResto);
       });
 
       form.addEventListener('submit', async (submitEvent) => { // async has to be declared all the way to get an await
@@ -65,8 +77,8 @@ function getRandomIntInclusive(min, max) {
         // console.log('form submission'); // this is substituting for a "breakpoint"
         // arrayFromJson.data - we're accessing a key called 'data' on the returned object
         // it contains all 1,000 records we need
-        const restoArray = restoArrayMake(arrayFromJson.data);
-        createHtmlList(restoArray);
+        currentArray = restoArrayMake(arrayFromJson.data);
+        createHtmlList(currentArray);
       });
     }
   }
