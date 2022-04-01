d4f917a76fdf2a9410a5f56be26dbe44b2a6ed77
sravvel <98434706+sravvel@users.noreply.github.com>
2022-04-01 19:36:19 -0400

    basic flappy controls

---
+++ client/flappy/script.js
@@ -0,0 +1,40 @@
+document.addEventListener ('DOMContentLoaded' , () => {
+    const bird = document.querySelector('.bird');
+    const gameDisplay = document.querySelector('.game_container');
+    const ground = document.querySelector('.ground');
+
+    let birdLeft = 220;
+    let birdBottom = 100;
+    let gravity = 2;
+
+    function startGame() {
+        birdBottom -= gravity;
+        bird.style.bottom = birdBottom + 'px';
+        bird.style.left = birdLeft + 'px';
+    }
+    let timerId = setInterval(startGame, 20);
+
+    function control(e){
+        if (e.keyCode === 32) {
+            jump();
+        }
+    }
+
+    function jump() {
+       if (birdBottom < 500) birdBottom += 50;
+        bird.style.bottom = birdBottom + 'px';
+    }
+    document.addEventListener('keyup', control);
+
+
+function generateObstacle{ 
+
+}
+
+
+
+
+
+
+
+});
